340d12a430a72feb1c67f1099f41d43a1a36e5c3
DNechev <dido94m@gmail.com>
2021-06-27 13:08:00 +0200

    Entities added, Redis configuration

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+            this.ShoppingCartItems = new List<ShoppingCartItem>();
+        }
+
+        public ShoppingCart(string username) 
+            : this()
+        {
+            this.Username = username;
+        }
+
+        public string Username { get; set; }
+
+        public ICollection<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public decimal TotalPrice {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in ShoppingCartItems)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
