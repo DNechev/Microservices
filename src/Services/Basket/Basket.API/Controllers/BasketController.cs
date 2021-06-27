cd0d270f095edfdaf5cfdd9da3ad4ebcc34f84ca
DNechev <dido94m@gmail.com>
2021-06-27 13:47:09 +0200

    Implemented controller, registered dependencies

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,38 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet("{username}", Name = "GetBasket")]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+        {
+            return Ok(await _basketRepository.GetBasket(username) ?? new ShoppingCart(username));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _basketRepository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{username}", Name = "DeleteBasket")]
+        public async Task<ActionResult> DeleteBasket(string username)
+        {
+            await _basketRepository.DeleteBasket(username);
+            return Ok();
+        }
+    }
+}
